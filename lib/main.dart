f651dd7a8af75461e8d5ca2cb746cbb260b4c6a7
Naufaldi Egaputra R <faldiega@gmail.com>
2019-06-24 16:42:02 +0700

    membuat tap bar

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import './hal_komputer.dart' as pc;
+import './hal_headset.dart' as hs;
+import './hal_radio.dart' as rdo;
+import './hal_smartphone.dart' as hp;
+
+void main() {
+  runApp(new MaterialApp(
+    title: "Tap Bar",
+    home: new Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin{
+
+  TabController controller;
+@override
+  void initState() {
+    controller = new TabController(vsync: this, length: 4);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Colors.amber,
+        title: new Text("Daftar Elektronik"),
+        bottom: new TabBar(
+          controller: controller,
+          tabs: <Widget>[
+            new Tab(icon: new Icon(Icons.computer),),
+            new Tab(icon: new Icon(Icons.headset),),
+            new Tab(icon: new Icon(Icons.radio),),
+            new Tab(icon: new Icon(Icons.smartphone),),
+          ],
+        ),
+      ),
+      body: new TabBarView(
+        controller: controller,
+        children: <Widget>[
+          new pc.Komputer(),
+          new hs.Headset(),
+          new rdo.Radio(),
+          new hp.Smartphone(),
+        ],
+      ),
+    );
+  }
+}
